897c2b342fa86cbad041ed671d799b2a365b5d65
Alexander Shulyak <acshulyak@utexas.edu>
2017-01-31 09:19:26 -0600

    added query plan queries. made a run script for collecting logs and query plans. updated io run script name.

--- perf/run.sh
+++ perf/run.sh
@@ -2,7 +2,7 @@
 source perf.conf
 
 #queries=("q3.sql" "q6.sql" "q14.sql" "q19.sql")
-queries=("q3.sql")
+queries=("q1.sql")
 for query in ${queries[@]}; do
 # run bench
 echo $query
@@ -16,8 +16,8 @@ for p_test in ${PERF_TESTS[@]}; do
   echo "${query:0:3}.tab" 
   # pre-running
   #$PERF_CMD -a -e ${PERF_TESTS[$I]} -o "$PERF_DIR/mysql_${query}_perf_${PERF_T_N}" -- mysql -u root --password=\!ong\-horn tpch < $1$query > ${query:0:3}.tab
-  $PERF_CMD -p $2 -e ${PERF_TESTS[$I]} -o "$PERF_DIR/mysql_${query}_perf_${PERF_T_N}" -- mysql -u root --password=\!ong\-horn tpch < $1$query > ${query:0:3}.tab
-  #mysql -u root --password=\!ong\-horn tpch < $1$query > ${query:0:3}.tab
+  #$PERF_CMD -p $2 -e ${PERF_TESTS[$I]} -o "$PERF_DIR/mysql_${query}_perf_${PERF_T_N}" -- mysql -u root --password=\!ong\-horn tpch < $1$query > ${query:0:3}.tab
+  mysql -u root --password=\!ong\-horn tpch < $1$query > ${query:0:3}.tab
 # post-running
   I=$I+1
 done
